79544cef20e5e000cbe5dca5e3da16ff90753001
Nicolas Lœuillet <nicolas@loeuillet.org>
2017-02-28 21:53:43 +0100

    Cleaned up code with php-cs-fixer

--- MaTPEBundle.php
+++ MaTPEBundle.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tilotiti
- * Date: 20/02/2017
- * Time: 16:14
- */
 
 namespace Tiloweb\MaTPEBundle;
 
@@ -12,5 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MaTPEBundle extends Bundle
 {
-
 }
--- Service/MaTPEService.php
+++ Service/MaTPEService.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tilotiti
- * Date: 20/02/2017
- * Time: 17:35
- */
 
 namespace Tiloweb\MaTPEBundle\Service;
 
@@ -23,31 +17,30 @@ class MaTPEService
         $this->firm = $firm;
     }
 
-    private function request($method, $endpoint, $data = array()) {
-
-
-        switch($method) {
-            case "POST":
-            case "PATCH":
-            case "DELETE":
+    private function request($method, $endpoint, $data = array())
+    {
+        switch ($method) {
+            case 'POST':
+            case 'PATCH':
+            case 'DELETE':
                 $curl = curl_init(self::API_URL.$this->firm.$endpoint.'.json');
 
                 curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
                 curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
                 break;
-            case "GET":
+            case 'GET':
             default:
                 $curl = curl_init(self::API_URL.$this->firm.$endpoint.'.json'.'?'.http_build_query($data));
 
-                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
                 break;
         }
 
-        curl_setopt($curl, CURLOPT_USERPWD, $this->login . ":" . $this->key);
+        curl_setopt($curl, CURLOPT_USERPWD, $this->login.':'.$this->key);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
             'User-Agent: Fidcar.com (thibault@fidcar.com)',
-            'Content-type: application/json; charset=utf-8'
+            'Content-type: application/json; charset=utf-8',
         ));
 
         $response = curl_exec($curl);
@@ -56,22 +49,21 @@ class MaTPEService
 
         curl_close($curl);
 
-        if($response) {
+        if ($response) {
             $req_json = json_decode($response, true);
 
-            if(isset($req_json['errors'])) {
-                 foreach($req_json['errors'] as $error => $message) {
-                    throw new \Exception($error." : ".$message[0]." ".self::API_URL.$this->firm.$endpoint.'.json'.'?'.http_build_query($data));
+            if (isset($req_json['errors'])) {
+                foreach ($req_json['errors'] as $error => $message) {
+                    throw new \Exception($error.' : '.$message[0].' '.self::API_URL.$this->firm.$endpoint.'.json'.'?'.http_build_query($data));
                 }
             } else {
                 return $req_json;
             }
         } else {
-            if($http_code != 200) {
-                throw new \Exception("Empty response ($http_code) : ".$method." ".self::API_URL.$this->firm.$endpoint.'.json'.'?'.http_build_query($data));
+            if ($http_code != 200) {
+                throw new \Exception("Empty response ($http_code) : ".$method.' '.self::API_URL.$this->firm.$endpoint.'.json'.'?'.http_build_query($data));
             }
         }
-
     }
 
     /*
@@ -79,38 +71,49 @@ class MaTPEService
      */
 
     /**
-     * List MaTPE Customers
+     * List MaTPE Customers.
+     *
      * @return array
      */
-    public function listCustomers() {
+    public function listCustomers()
+    {
         return $this->request('GET', '/customers');
     }
 
     /**
-     * Create a new customer on MaTPE
+     * Create a new customer on MaTPE.
+     *
      * @param array $data
+     *
      * @return array
      */
-    public function createCustomer(array $data) {
+    public function createCustomer(array $data)
+    {
         return $this->request('POST', '/customers', $data);
     }
 
     /**
-     * Get a customer from MaTPE
+     * Get a customer from MaTPE.
+     *
      * @param $id
+     *
      * @return array
      */
-    public function getCustomer($id) {
+    public function getCustomer($id)
+    {
         return $this->request('GET', '/customers/'.$id);
     }
 
     /**
-     * Update a customer from MaTPE
+     * Update a customer from MaTPE.
+     *
      * @param $id
      * @param array $data
+     *
      * @return array
      */
-    public function updateCustomer($id, array $data) {
+    public function updateCustomer($id, array $data)
+    {
         $this->request('PATCH', '/customers/'.$id, $data);
 
         return $this->getCustomer($id);
@@ -121,14 +124,17 @@ class MaTPEService
      */
 
     /**
-     * List all invoices on MaTPE
+     * List all invoices on MaTPE.
+     *
      * @param $customer integer
+     *
      * @return array
      */
-    public function listInvoices($customer = 0) {
-        if($customer) {
+    public function listInvoices($customer = 0)
+    {
+        if ($customer) {
             return $this->request('GET', '/invoices', array(
-                'customer_id' => $customer
+                'customer_id' => $customer,
             ));
         } else {
             return $this->request('GET', '/invoices');
@@ -136,13 +142,16 @@ class MaTPEService
     }
 
     /**
-     * Create an invoice in Quipu
+     * Create an invoice in Quipu.
+     *
      * @param $customer
      * @param array $data
      * @param array $items
+     *
      * @return array
      */
-    public function createInvoice($customer, array $data, array $items) {
+    public function createInvoice($customer, array $data, array $items)
+    {
         $data['customer_id'] = $customer;
         $data['items'] = $items;
 
@@ -150,21 +159,27 @@ class MaTPEService
     }
 
     /**
-     * Get an Invoice on MaTPE
+     * Get an Invoice on MaTPE.
+     *
      * @param $id int
+     *
      * @return array
      */
-    public function getInvoice($id) {
+    public function getInvoice($id)
+    {
         return $this->request('GET', '/invoices/'.$id);
     }
 
     /**
-     * Update attributes invoice on MaTPE
+     * Update attributes invoice on MaTPE.
+     *
      * @param $id
      * @param array $data
+     *
      * @return array
      */
-    public function updateInvoice($id, array $data) {
+    public function updateInvoice($id, array $data)
+    {
         $this->request('PATCH', '/invoices/'.$id, $data);
 
         return $this->getInvoice($id);
